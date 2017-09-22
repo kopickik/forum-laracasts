2cff2f58e1e43924893e4a924e54357398e22075
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-09-22 15:12:52 -0400

    splitting single view from all view tests

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -14,8 +14,12 @@ class ThreadsTest extends TestCase {
     $response = $this->get('/threads');
     $response->assertStatus(200);
     $response->assertSee($thread->title);
-
-    $response = $this->get('/threads' . $thread->id);
+  }
+  
+  public function test_a_user_can_browse_a_single_thread() {
+    $thread = factory('App\Thread')->create();
+    $response = $this->get('/threads/' . $thread->id);
     $response->assertSee($thread->title);
   }
+
 }
