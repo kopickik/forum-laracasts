59472ef0a27b4525b741693f42d36e9bf522e05b
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-09-22 15:48:57 -0400

    cleanup and refactoring

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -31,4 +31,12 @@ class ThreadsTest extends TestCase {
       ->assertSee($reply->body);
   }
 
+  public function test_a_thread_has_replies() {
+    $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
+  }
+
+  public function test_a_thread_has_a_creator() {
+    $this->assertInstanceOf('App\User', $this->thread->creator);
+  }
+
 }
