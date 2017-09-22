ccfcb3e7b7e56f020d0dd99cd90ce965b7ccca66
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-09-22 15:20:13 -0400

    refactoring threads test

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -9,17 +9,20 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class ThreadsTest extends TestCase {
   use DatabaseMigrations;
 
+  public function setUp() {
+    parent::setUp();
+    $this->thread = factory('App\Thread')->create();
+  }
+
   public function test_a_user_can_browse_threads() {
-    $thread = factory('App\Thread')->create();
-    $response = $this->get('/threads');
-    $response->assertStatus(200);
-    $response->assertSee($thread->title);
+    $this->get('/threads')
+      ->assertStatus(200)
+      ->assertSee($this->thread->title);
   }
   
   public function test_a_user_can_browse_a_single_thread() {
-    $thread = factory('App\Thread')->create();
-    $response = $this->get('/threads/' . $thread->id);
-    $response->assertSee($thread->title);
+    $this->get('/threads/' . $this->thread->id)
+      ->assertSee($this->thread->title);
   }
 
 }
