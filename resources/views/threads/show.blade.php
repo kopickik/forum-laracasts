aa1fe2c870096d5cdb963cacac20306552fbc620
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-09-22 14:59:38 -0400

    fixing show view

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,9 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">{{ $thread->title }}</div>
-                <div class="panel-body">
-                blah
-                </div>
+                <div class="panel-body">{{ $thread->body }}</div>
             </div>
         </div>
     </div>
