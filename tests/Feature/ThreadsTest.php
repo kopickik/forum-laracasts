1b93f2e60abee664d2704e28fa82f8006f2eb47c
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-09-25 17:51:40 -0400

    adding replies test to ThreadsTest.php

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -40,4 +40,14 @@ class ThreadsTest extends TestCase {
     $this->assertInstanceOf('App\User', $this->thread->creator);
   }
 
+  /** @test */
+  function a_thread_can_add_a_reply()
+  {
+    $this->thread->addReply([
+      'body' => 'Foobar',
+      'user_id' => 1
+    ]);
+    $this->assertCount(1, $this->thread->replies);
+  }
+
 }
